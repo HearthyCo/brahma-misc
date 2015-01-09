21d1cc52a541cdc3982a77331c83bb0f2f3e235f
Ignacio Lago <ignacio@glue.gl>
2015-01-09 14:15:40 +0100

    brahma-git.fish

--- fish/brahma-git.fish
+++ fish/brahma-git.fish
@@ -4,9 +4,11 @@ set -l color_1 '\033[0;36m'
 set -l color_2 '\033[1;36m'
 set -l color_no '\033[0m'
 
-for dir in brahma-*/
-  cd $dir
-  echo -s -e "$color_1" "$dir" "\$ " "$color_2" "git $argv" "$color_no"
-  git $argv
-  cd ..
+if test "$argv"
+  for dir in brahma-*/
+    cd $dir
+    echo -s -e "$color_1" "$dir" "\$ " "$color_2" "git $argv" "$color_no"
+    git $argv
+    cd ..
+  end
 end
