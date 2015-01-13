dce8bdbda9f282512088ae5c49fc299536a9a1d2
Anxo Soto <anxo@glue.gl>
2015-01-13 13:32:54 +0100

    Bashing the fish

---
+++ bash/brahma-git.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+color_1='\033[0;36m'
+color_2='\033[1;36m'
+color_no='\033[0m'
+
+if test "$@"; then
+  for dir in brahma-*/; do
+    cd $dir
+    echo -e "$color_1$dir\$ ${color_2}git $@$color_no"
+    git "$@"
+    cd ..
+  done
+fi
---
+++ bash/brahma-grunt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+color_1='\033[0;33m'
+color_2='\033[1;33m'
+color_no='\033[0m'
+
+# This should be in dependency order, even if duplication is needed
+tasks="css components client"
+
+for task in $tasks; do
+  cd "brahma-$task"
+  echo -e "${color_1}brahma-$task\$ ${color_2}grunt $argv$color_no"
+  grunt $argv
+  cd ..
+done
