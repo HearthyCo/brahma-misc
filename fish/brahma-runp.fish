be3c85826dc7f3e6eac70eff6b02684378918ca4
Ignacio Lago <ignacio@glue.gl>
2015-01-14 17:57:35 +0100

    run*.fish: check if package.json for npm calls, bower.json for bower

--- fish/brahma-runp.fish
+++ fish/brahma-runp.fish
@@ -6,6 +6,12 @@ set -l color_no '\033[0m'
 
 if test "$argv"
   for dir in brahma-*/
+    if test "npm" = $argv[1] -a ! -f "$dir/package.json"
+      continue
+    end
+    if test "bower" = $argv[1] -a ! -f "$dir/bower.json"
+      continue
+    end
     cd $dir
     echo -s -e "$color_1" "$dir" "\$ " "$color_2" "$argv" "$color_no"
     eval "$argv" &
