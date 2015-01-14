ec98ca288ae1a65440e8abb281c49fa657c7681c
Anxo Soto <anxo@glue.gl>
2015-01-14 11:54:44 +0100

    Mejor check de argv para bash.

--- bash/brahma-runp.sh
+++ bash/brahma-runp.sh
@@ -4,7 +4,7 @@ color_1='\033[0;32m'
 color_2='\033[1;32m'
 color_no='\033[0m'
 
-if test "$@"; then
+if (( "$#" != 0 )); then
   for dir in brahma-*/; do
     cd $dir
     echo -e "$color_1$dir\$ $color_2$@$color_no"
