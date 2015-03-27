7264461cfd950e3ca8adb95a6de29be4639f8c3a
Ignacio Lago <ignacio@glue.gl>
2015-03-27 14:27:47 +0100

    Multi-thread grunt and deploy

--- bash/brahma-deploy.bash
+++ bash/brahma-deploy.bash
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-color_1='\033[0;37m'
-color_2='\033[1;37m'
+# for i in $n_procs; do
+#   ./procs[${i}] &
+#   pids[${i}]=$!
+# done
+#
+# for pid in ${pids[*]}; do
+#   wait $pid
+# done
+
+color_1='\033[0;35m'
+color_2='\033[1;35m'
+color_3='\033[0;90m'
 color_no='\033[0m'
 
 echo -e "${color_1}${color_2} ____  _____ _____ __    _____ __ __    _____ _____ _____ _____ _____ _____
@@ -10,17 +20,20 @@ echo -e "${color_1}${color_2} ____  _____ _____ __    _____ __ __    _____ _____
 |____/|_____|__|  |_____|_____| |_|    |_____| |_| |__|__|__|__| |_| |_____|$color_no"
 
 ./grunt
-cd "hearthy-client-public"
-python -c'import time; print repr(time.time())' > .force_push
-git add .
-git commit --amend -a -m "deployment" > /dev/null
-git push --force
-cd ..
-cd "hearthy-professional-public"
-python -c'import time; print repr(time.time())' > .force_push
-git add .
-git commit --amend -a -m "deployment" > /dev/null
-git push --force
-cd ..
+for project in "brahma-api" "brahma-chat" "brahma-notifications"; do
+  cd "$project"
+  echo -e "${color_1}Deploying ${color_2}$project$color_no"
+  git push --force DEV develop:master
+  cd ..
+done
+for project in "hearthy-client-public" "hearthy-professional-public" "brahma-admin-public"; do
+  cd "$project"
+  echo -e "${color_1}Deploying ${color_2}$project$color_no"
+  python -c'import time; print repr(time.time())' > .force_push
+  git add .
+  git commit --amend -a -m "deployment" > /dev/null
+  git push --force
+  cd ..
+done
 
 echo -e "${color_1}${color_2}END OF DEPLOYMENT$color_no"
--- bash/brahma-grunt.bash
+++ bash/brahma-grunt.bash
@@ -2,14 +2,25 @@
 
 color_1='\033[0;33m'
 color_2='\033[1;33m'
+color_3='\033[0;90m'
 color_no='\033[0m'
 
 # This should be in dependency order, even if duplication is needed
-tasks="brahma-css brahma-components hearthy-client hearthy-professional"
+tasks="brahma-css brahma-components"
+projects="hearthy-client hearthy-professional brahma-admin brahma-notifications"
 
 for task in $tasks; do
   cd "$task"
   echo -e "${color_1}$task\$ ${color_2}grunt $argv$color_no"
-  grunt $argv
+  grunt $argv | sed "s/^/$(echo -e "$color_3""$task" \> "$color_no")/" &
   cd ..
 done
+wait
+
+for project in $projects; do
+  cd "$project"
+  echo -e "${color_1}$project\$ ${color_2}grunt $argv$color_no"
+  grunt $argv | sed "s/^/$(echo -e "$color_3""$project" \> "$color_no")/" &
+  cd ..
+done
+wait
